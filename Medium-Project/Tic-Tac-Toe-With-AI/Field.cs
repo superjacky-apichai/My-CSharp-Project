fd83309718b523fbb0b12d824b1a3f6a61ed6674
Apichai gusaljeerung <71862279+superjacky-apichai@users.noreply.github.com>
2022-04-30 18:17:36 +0700

    Add files via upload

---
+++ Medium-Project/Tic-Tac-Toe-With-AI/Field.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Tic_Tac_Toe_With_AI
+{
+    internal class Field
+    {
+
+        public string[,] field ={ { "1", "2", "3" },
+        {"4","5","6"},
+        {"7","8","9" }};
+
+        int count = 0;
+
+        public void PrintField()
+        {
+            System.Console.WriteLine("     |     |  ");
+            System.Console.WriteLine("  {0}  |  {1}  |  {2}   ", field[0, 0], field[0, 1], field[0, 2]);
+            System.Console.WriteLine("_____|_____|_____  ");
+            System.Console.WriteLine("     |     |  ");
+            System.Console.WriteLine("  {0}  |  {1}  |  {2}   ", field[1, 0], field[1, 1], field[1, 2]);
+            System.Console.WriteLine("_____|_____|_____  ");
+            System.Console.WriteLine("     |     |  ");
+            System.Console.WriteLine("  {0}  |  {1}  |  {2}   ", field[2, 0], field[2, 1], field[2, 2]);
+            System.Console.WriteLine("     |     |       ");
+
+        }
+
+
+
+        public void SetField(bool isXOrO, int toPosiTion)
+        {
+
+            switch (toPosiTion)
+            {
+                case 1: field[0, 0] = isXOrO ? "X" : "O"; count++; break;
+                case 2: field[0, 1] = isXOrO ? "X" : "O"; count++; break;
+                case 3: field[0, 2] = isXOrO ? "X" : "O"; count++; break;
+                case 4: field[1, 0] = isXOrO ? "X" : "O"; count++; break;
+                case 5: field[1, 1] = isXOrO ? "X" : "O"; count++; break;
+                case 6: field[1, 2] = isXOrO ? "X" : "O"; count++; break;
+                case 7: field[2, 0] = isXOrO ? "X" : "O"; count++; break;
+                case 8: field[2, 1] = isXOrO ? "X" : "O"; count++; break;
+                case 9: field[2, 2] = isXOrO ? "X" : "O"; count++; break;
+
+                default: System.Console.WriteLine("Out of range pleases put number 1-9"); break;
+            }
+        }
+
+
+        public bool IsDumplicate(int toPosiTion)
+        {
+            switch (toPosiTion)
+            {
+                case 1: if (field[0, 0] == "X" || field[0, 0] == "O") { Console.WriteLine("this field was ocupied! please choose another field"); return false; } break;
+                case 2: if (field[0, 1] == "X" || field[0, 1] == "O") { Console.WriteLine("this field was ocupied! please choose another field"); return false; } break;
+                case 3: if (field[0, 2] == "X" || field[0, 2] == "O") { Console.WriteLine("this field was ocupied! please choose another field"); return false; } break;
+                case 4: if (field[1, 0] == "X" || field[1, 0] == "O") { Console.WriteLine("this field was ocupied! please choose another field"); return false; } break;
+                case 5: if (field[1, 1] == "X" || field[1, 1] == "O") { Console.WriteLine("this field was ocupied! please choose another field"); return false; } break;
+                case 6: if (field[1, 2] == "X" || field[1, 2] == "O") { Console.WriteLine("this field was ocupied! please choose another field"); return false; } break;
+                case 7: if (field[2, 0] == "X" || field[2, 0] == "O") { Console.WriteLine("this field was ocupied! please choose another field"); return false; } break;
+                case 8: if (field[2, 1] == "X" || field[2, 1] == "O") { Console.WriteLine("this field was ocupied! please choose another field"); return false; } break;
+                case 9: if (field[2, 2] == "X" || field[2, 2] == "O") { Console.WriteLine("this field was ocupied! please choose another field"); return false; } break;
+
+                default: System.Console.WriteLine("Out of range pleases put number 1-9"); return false;
+            }
+
+            return true;
+        }
+
+        public bool IsFinish()
+        {
+            if (WhoWin())
+            {
+                return true;
+            }
+            else if (!WhoWin() && count == 9)
+            {
+                Console.Clear();
+                PrintField();
+                System.Console.WriteLine("DRAW!");
+                return true;
+            }
+            return false;
+        }
+
+        private bool WhoWin()
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[0, i] == "X" && field[1, i] == "X" && field[2, i] == "X" || field[i, 0] == "X" && field[i, 1] == "X" && field[i, 2] == "X")
+                {
+                    Console.Clear();
+                    PrintField();
+                    System.Console.WriteLine("X WIN!!!");
+                    return true;
+                }
+                else if (field[0, i] == "O" && field[1, i] == "O" && field[2, i] == "O" || field[i, 0] == "O" && field[i, 1] == "O" && field[i, 2] == "O")
+                {
+                    Console.Clear();
+                    PrintField();
+                    System.Console.WriteLine("O WIN!!!");
+                    return true;
+                }
+            }
+
+            if (field[0, 0] == "X" && field[1, 1] == "X" && field[2, 2] == "X" || field[0, 2] == "X" && field[1, 1] == "X" && field[2, 0] == "X")
+            {
+                Console.Clear();
+                PrintField();
+                System.Console.WriteLine("X WIN!!!");
+                return true;
+            }
+            else if (field[0, 0] == "O" && field[1, 1] == "O" && field[2, 2] == "O" || field[0, 2] == "O" && field[1, 1] == "O" && field[2, 0] == "O")
+            {
+                Console.Clear();
+                PrintField();
+                System.Console.WriteLine("O WIN!!!");
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ Medium-Project/Tic-Tac-Toe-With-AI/Program.cs
@@ -0,0 +1,236 @@
+﻿using System;
+
+namespace Tic_Tac_Toe_With_AI
+{
+    internal class Program
+    {
+        //create the field and player object
+        static Player player1;
+        static Player player2;
+        static Field field = new Field();
+
+        static void Main(string[] args)
+        {
+
+            //fisrt loop for reset the game and exits game
+            while (true)
+            {
+                Console.Clear();
+
+                field = new Field();
+                //this array value assinge frome interfaceForChooseUser function
+                int[] player = interfaceForChooseUser();
+                //this fucntion assinge value of object player1 and object player 2
+                ChooseUser(player);
+                bool turnChecker = true;
+
+                System.Threading.Thread.Sleep(1000);
+
+                //second loop for check if game Ain't Over Yet this loop still continue
+                while (!field.IsFinish())
+                {
+
+                    field.PrintField();
+
+                    if (turnChecker)
+                    {
+                        Console.Write("User X Enter your field: ");
+                        int setPosition = player1.SetPosition();
+                        while (!field.IsDumplicate(setPosition) || (setPosition < 1 || setPosition > 9))
+                        {
+                            Console.Write("User X Enter your field again: ");
+                            setPosition = player1.SetPosition();
+                        }
+
+                        field.SetField(true, setPosition);
+                        turnChecker = false;
+                    }
+                    else if (!turnChecker)
+                    {
+                        Console.Write("User O Enter your field: ");
+                        int setPosition = player2.SetPosition();
+
+                        while (!field.IsDumplicate(setPosition) || (setPosition < 1 || setPosition > 9))
+                        {
+
+                            Console.Write("User O Enter your field again: ");
+                            setPosition = player2.SetPosition();
+                        }
+                        field.SetField(false, setPosition);
+
+                        turnChecker = true;
+                    }
+
+                    if (field.IsFinish())
+                    {
+
+                        System.Threading.Thread.Sleep(5000);
+                    }
+                    Console.Clear();
+
+                }
+
+
+                Console.Clear();
+
+                Console.WriteLine("Reset the game or exits the programe?");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine("1.Reset the game!");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine("2.Exits the program!");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("Enter your input: ");
+                int resetOrExit = 0;
+                while (!int.TryParse(Console.ReadLine(), out resetOrExit) || resetOrExit < 0 || resetOrExit > 2)
+                {
+
+                    if (resetOrExit < 0 || resetOrExit > 2)
+                    {
+                        Console.WriteLine("Please enter number in range 1 - 2!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong input.");
+                    }
+
+                }
+
+                if (resetOrExit == 1)
+                {
+
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+
+        //Method for start menu game
+        public static int[] interfaceForChooseUser()
+        {
+            //create interface menu game player choose
+            Console.WriteLine("Welcome to Tic-Tac-Toe!!!");
+
+            System.Threading.Thread.Sleep(1000);
+
+            //Let X player choose the user.
+            Console.WriteLine("Choose for user X");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("1.For Player");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("2.For AI-Easy");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("3.For AI-Medium");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("4.For AI-Hard");
+            System.Threading.Thread.Sleep(1000);
+
+
+            //made input for user X
+            int inputUserX = 0;
+            Console.Write("Enter your input: ");
+            while (!int.TryParse(Console.ReadLine(), out inputUserX) || (inputUserX > 4 || inputUserX < 1))
+            {
+                if ((inputUserX > 4 || inputUserX < 1))
+                {
+                    Console.WriteLine("please enter number in range 1 - 4");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input!");
+                }
+                Console.Write("Enter your input: ");
+            }
+
+            switch (inputUserX)
+            {
+                case 1: Console.WriteLine("User X choose player!"); break;
+                case 2: Console.WriteLine("User X choose AI-Easy!"); break;
+                case 3: Console.WriteLine("User X choose AI-Medium!"); break;
+                case 4: Console.WriteLine("User X choose AI-Hard!"); break;
+            }
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+
+            //Let O player choose the user.
+            Console.WriteLine("Choose for user O");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("1.For Player");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("2.For AI-Easy");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("3.For AI-Medium");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("4.For AI-Hard");
+            System.Threading.Thread.Sleep(1000);
+
+
+            //made input for user O
+            int inputUserO = 0;
+            Console.Write("Enter your input: ");
+            while (!int.TryParse(Console.ReadLine(), out inputUserO) || (inputUserO > 4 || inputUserO < 1))
+            {
+
+                if ((inputUserO > 4 || inputUserO < 1))
+                {
+                    Console.WriteLine("please enter number in range 1 - 4");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input!");
+                }
+                Console.Write("Enter your input: ");
+            }
+
+            switch (inputUserO)
+            {
+                case 1: Console.WriteLine("User O choose player!"); break;
+                case 2: Console.WriteLine("User O choose AI-Easy!"); break;
+                case 3: Console.WriteLine("User O choose AI-Medium!"); break;
+                case 4: Console.WriteLine("User O choose AI-Hard!"); break;
+            }
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+
+            //return array for value of user x and o 
+            return new int[] { inputUserX, inputUserO };
+        }
+
+
+
+        //Use Polymorphic Parameters to define user
+        public static void ChooseUser(int[] player)
+        {
+
+
+            switch (player[0])
+            {
+                case 1: player1 = new Player(); break;
+                case 2: player1 = new AI_Easy(); break;
+                case 3: player1 = new AI_Medium(field, true); break;
+                case 4: player1 = new AI_Hard(field, true); break;
+            }
+
+            switch (player[1])
+            {
+                case 1: player2 = new Player(); break;
+                case 2: player2 = new AI_Easy(); break;
+                case 3: player2 = new AI_Medium(field, false); break;
+                case 4: player2 = new AI_Hard(field, false); break;
+            }
+        }
+    }
+}
